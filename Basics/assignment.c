2c2b4e6ac6062c409242c08d5e17d9185ad68981
Your Name <you@example.com>
2026-08-28 01:31:28 +0600

    w3

---
+++ Basics/assignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   float celsius,f;
+    scanf("%f", &celsius);
+    f=((celsius*9)/5)+32;
+
+    printf("%.1f Celsius = %.1f Farenheit",celsius,f);// Calculate fahrenheit
+
+
+    // Calculate fahrenheit
+
+    // Print the results
+    return 0;
+}
