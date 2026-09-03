0af5e185a7bfa4400ca5ab4e92aaf6ea7d03817a
Your Name <you@example.com>
2026-09-04 01:01:30 +0600

    (switch)

---
+++ loop/w3_switch.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int choice=1;
+    switch(choice)
+{
+    case 1:
+    printf("You ordered Coffee");
+    break;
+    case 2:
+    printf("You ordered Tea");
+    break;
+    default:
+    printf("Invalid choice");
+
+    return 0;
+}}
