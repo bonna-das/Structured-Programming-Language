a9dffc85be5769d6fc17ef42c911755060c0f8d8
Your Name <you@example.com>
2026-09-05 05:38:30 +0600

    (fact)

--- Function.c/fact.c
+++ Function.c/fact.c
@@ -1,2 +1,16 @@
 #include<stdio.h>
+int fact(int n){
+    int fact=1;
+    for (int i=1;i<=n;i++){
+        fact=fact*i;
+
+    }
+    return fact;
+}    
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d",fact(a));
+}
+
 
