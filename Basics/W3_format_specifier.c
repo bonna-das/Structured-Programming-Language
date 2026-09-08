a5485264eb2637e4d5f2b6bcd610f9838d759d70
Your Name <you@example.com>
2026-09-08 21:09:11 +0600

    Assignment

---
+++ Basics/W3_format_specifier.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int myNum=20;
+    float myFloat=1.09;
+    char myLetter='Y';
+    printf("%d\n",myNum);
+    printf("%.2f\n",myFloat);
+    printf("%c",myLetter);
+    return 0;
+}
