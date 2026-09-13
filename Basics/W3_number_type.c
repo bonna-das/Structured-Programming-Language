c56a2fcdd83dbd692e74e7664bb692d97412cb8c
Your Name <you@example.com>
2026-09-13 23:03:04 +0600

    Assignment

---
+++ Basics/W3_number_type.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float myFloatnumber=3.5;
+    printf("%f\n",myFloatnumber);//6 digits after decimal
+    printf("%.1f\n",myFloatnumber);
+    printf("%.2f\n",myFloatnumber);
+    printf("%.3f\n",myFloatnumber);
+    return 0;
+}
+
+
