49be29834a27d9279043407b7fefcfde177907c3
Your Name <you@example.com>
2026-09-13 23:20:19 +0600

    Assignment

---
+++ Basics/W3_sizeof_operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int A;
+    float B;
+    double C;
+    char D;
+    printf("Size of int %zu\n",sizeof(A));
+    printf("Size of float %zu\n",sizeof(B));
+    printf("Size of double %zu\n",sizeof(C));
+    printf("Size of char %zu\n",sizeof(D));
+    return 0;
+}
